bd417e5225fc15fc87985d45c66872b6e2d8aa81
Arianul Islam <islamarianul@gmail.com>
2018-07-25 04:07:16 +0000

    Class work 8 done

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 var http = require('http');
+//var mongoose = require ('mongoose');
+var mongo=require('mongodb');
   var express = require('express');
   var app = express();
   var bodyParser = require('body-parser');
-  var server = http.Server(app);
+ 
+  
+var db, uri = "mongodb://" + process.env.IP + "/test";
+   
+  mongo.MongoClient.connect(uri,{userNewUrlParser:true},function(err,client){
+    if(err){console.log("Could not connect to MongoDB")
+    }
+    else {
+      db = client.db('simplenode');
+      console.log("Database Created")
+    }
+  });
+   
+  var save = function(form_data){
+    db.createCollection('users',function(err,collection){ if (err) throw err;});
+    var collection = db.collection('users');
+    collection.save(form_data);
+    
+  }
+  
+ var server = http.Server(app);
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
-
   
-
   app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
   })
@@ -26,14 +46,13 @@ var http = require('http');
     console.log("post received: %s %s", username, email);
 });
 
-app.post('/submit_user', function(req, res){
+app.post('/submit_user',function(req, res){
   console.log(req.body);
-})
+   save(req.body);
+  res.status('200');
+});
 
-
-
-  
-  
+ 
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
