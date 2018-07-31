4133520d16782af1777f744ce6ae65c845594d54
Arianul Islam <islamarianul@gmail.com>
2018-07-31 18:55:48 +0000

    classwork 9 done

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 var http = require('http');
-//var mongoose = require ('mongoose');
+var mongoose = require ('mongoose');
 var mongo=require('mongodb');
   var express = require('express');
   var app = express();
   var bodyParser = require('body-parser');
  
   
-var db, uri = "mongodb://" + process.env.IP + "/test";
+var db, uri = mongoose.connect("mongodb://" + process.env.IP + "/test");
    
-  mongo.MongoClient.connect(uri,{userNewUrlParser:true},function(err,client){
-    if(err){console.log("Could not connect to MongoDB")
-    }
-    else {
-      db = client.db('simplenode');
-      console.log("Database Created")
-    }
-  });
-   
+  // mongo.MongoClient.connect(uri,{userNewUrlParser:true},function(err,client){
+  //   if(err){console.log("Could not connect to MongoDB")
+  //   }
+  //   else {
+  //     db = client.db('simplenode');
+  //     console.log("Database Created")
+  //   }
+  // });
+mongoose.connection.on('error',function(){
+   console.log('Could not connect to mongodb');
+ });
+ 
+ var userSchema = new mongoose.Schema({
+ name: {
+   type: String,
+   required: "Name is required"
+ },
+ email: String
+ });
+ 
+ var User = mongoose.model('User', userSchema);
+ 
   var save = function(form_data){
     db.createCollection('users',function(err,collection){ if (err) throw err;});
     var collection = db.collection('users');
